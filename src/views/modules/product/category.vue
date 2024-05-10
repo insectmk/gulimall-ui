5add1b746cf66598ca6c5bef5168c449affadc39
makuning <makun_ing@126.com>
2024-05-10 14:17:51 +0800

    优化树形分类控件的展示

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,5 +1,28 @@
 <template>
-  <el-tree :data="menus" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+  <el-tree :data="menus"
+           :props="defaultProps"
+           show-checkbox
+           :expand-on-click-node="false">
+    <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-button
+            v-if="node.level <= 2"
+            type="text"
+            size="mini"
+            @click="() => append(data)">
+            Append
+          </el-button>
+          <el-button
+            v-if="node.childNodes.length === 0"
+            type="text"
+            size="mini"
+            @click="() => remove(node, data)">
+            Delete
+          </el-button>
+        </span>
+      </span>
+  </el-tree>
 </template>
 
 <script>
@@ -14,6 +37,12 @@ export default {
     }
   },
   methods: {
+    append (data) {
+      console.log('append', data)
+    },
+    remove (node, data) {
+      console.log('remove', node, data)
+    },
     // 获取树形分类
     getCategoriesTree () {
       this.$http({
@@ -22,9 +51,6 @@ export default {
       }).then(({data}) => {
         this.menus = data.data
       })
-    },
-    handleNodeClick (data) {
-      console.log(data)
     }
   },
   created () {
