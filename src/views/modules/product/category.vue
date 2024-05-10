661ef7689e3e4bb3b6deff47676fdc6f1d382946
makuning <makun_ing@126.com>
2024-05-10 15:33:46 +0800

    增加商品分类菜单删除功能

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -2,6 +2,8 @@
   <el-tree :data="menus"
            :props="defaultProps"
            show-checkbox
+           node-key="catId"
+           :default-expanded-keys="expandedKeys"
            :expand-on-click-node="false">
     <span class="custom-tree-node" slot-scope="{ node, data }">
         <span>{{ node.label }}</span>
@@ -29,7 +31,11 @@
 export default {
   data () {
     return {
+      // 树形控件默认展开key
+      expandedKeys: [],
+      // 分类菜单
       menus: [],
+      // 树形控件对应属性名
       defaultProps: {
         children: 'children',
         label: 'name'
@@ -40,8 +46,35 @@ export default {
     append (data) {
       console.log('append', data)
     },
+    // 删除分类
     remove (node, data) {
-      console.log('remove', node, data)
+      let ids = [data.catId]
+
+      this.$confirm(`此操作将删除【${data.name}】分类，是否继续?`, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.$http({
+          url: this.$http.adornUrl('/product/category/delete'),
+          method: 'post',
+          data: this.$http.adornData(ids, false)
+        }).then(({data}) => {
+          this.$message({
+            type: 'success',
+            message: '分类删除成功!'
+          })
+          // 刷新菜单
+          this.getCategoriesTree()
+          // 设置默认展开的菜单
+          this.expandedKeys = [node.parent.data.catId]
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        })
+      })
     },
     // 获取树形分类
     getCategoriesTree () {
